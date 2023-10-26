be0685a009ae6703c9b93d1a889111821f2c64da
Srs_vesta <101057653+SurajSanap@users.noreply.github.com>
2023-10-26 20:58:24 +0530

    Add files via upload

---
+++ Jarvis/Brain2.py
@@ -0,0 +1,16 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+
+#VOICE
+voices = engine.getProperty('voices')       #getting details of current voice
+#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+engine.setProperty('voice', voices[1].id)   # Female
+""" RATE"""
+rate = engine.getProperty('rate')   # getting details of current speaking rate
+print (rate)                        #printing current voice rate
+engine.setProperty('rate', 125)     # setting up new voice rate
+
+engine.say("Audio")
+engine.runAndWait()
+
